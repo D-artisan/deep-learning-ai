41135973e113cead8bf888582e897f756521cf6e
d-artisan <dartisan.inc@gmail.com>
2025-08-01 15:46:33 +0100

    mcp-openai

---
+++ chatgpt/mcp-openai.py
@@ -0,0 +1,25 @@
+import os
+import openai
+from openai import OpenAI
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.environ['OPENAI_API_KEY']
+
+
+client = OpenAI()
+
+resp = client.responses.create(
+    model="gpt-4.1",
+    tools=[
+        {
+            "type": "mcp",
+            "server_label": "deepwiki",
+            "server_url": "https://mcp.deepwiki.com/mcp",
+            "require_approval": "never",
+        },
+    ],
+    input="What transport protocols are supported in the 2025-03-26 version of the MCP spec?",
+)
+
+print(resp.output_text)
